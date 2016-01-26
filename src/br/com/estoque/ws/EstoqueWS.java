821757cd75a71a888b719d0432fd4408d9cc7167
Odilon.Junior <odilon.computacao@live.com>
2016-01-25 22:11:13 -0200

    Create git ignore

--- src/br/com/estoque/ws/EstoqueWS.java
+++ src/br/com/estoque/ws/EstoqueWS.java
@@ -13,7 +13,26 @@ import java.util.List;
  * Created by odilon on 19/01/16.
  */
 @WebService
-public class EstoqueWS {
+public
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+class EstoqueWS {
 
     private ItemDao dao = new ItemDao();
 
